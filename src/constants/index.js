4343df46987e82cb3710b1bf8f0603a6522c7e0e
AnK <56575187+NaKMiers@users.noreply.github.com>
2022-05-01 13:21:08 +0700

    change api link

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,4 +2,5 @@ import md5 from 'md5'
 
 export const truePw = md5('321')
 
-export const API = 'http://localhost:3001'
+export const API = 'https://literature-authors.herokuapp.com'
+export const APIx = 'http://localhost:3001'
